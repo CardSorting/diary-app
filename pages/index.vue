139569ab59981e1f476c91f2bb5ef10fc73d93ad
PlayMoreTCG <playmoretcg@gmail.com>
2024-12-30 22:37:59 -0700

    index

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Redirecting...</div>
+</template>
+
+<script setup>
+import { useAuth } from '~/composables/useAuth'
+
+const { user } = useAuth()
+
+onMounted(() => {
+  if (user.value) {
+    navigateTo('/diary')
+  } else {
+    navigateTo('/login')
+  }
+})
+</script>
